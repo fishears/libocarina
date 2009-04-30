3b9af2d64617a607344d5ec8c3de50900bead6d3
johnphilpott@ntlworld.com <johnphilpott@ntlworld.com@65107bec-3562-11de-9f24-65e390db57b8>
2009-04-30 09:12:31 +0000

    first draft

---
+++ libocarina/nbproject/Makefile-Release.mk
@@ -0,0 +1,69 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=gcc
+CCC=g++
+CXX=g++
+FC=
+
+# Macros
+PLATFORM=GNU-Linux-x86
+
+# Include project Makefile
+include Makefile
+
+# Object Directory
+OBJECTDIR=build/Release/${PLATFORM}
+
+# Object Files
+OBJECTFILES= \
+	${OBJECTDIR}/libocarina.o
+
+# C Compiler Flags
+CFLAGS=
+
+# CC Compiler Flags
+CCFLAGS=
+CXXFLAGS=
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS=
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS}
+	${MAKE}  -f nbproject/Makefile-Release.mk dist/Release/${PLATFORM}/liblibocarina.a
+
+dist/Release/${PLATFORM}/liblibocarina.a: ${OBJECTFILES}
+	${MKDIR} -p dist/Release/${PLATFORM}
+	${RM} dist/Release/${PLATFORM}/liblibocarina.a
+	${AR} rv dist/Release/${PLATFORM}/liblibocarina.a ${OBJECTFILES} 
+	$(RANLIB) dist/Release/${PLATFORM}/liblibocarina.a
+
+${OBJECTDIR}/libocarina.o: libocarina.c 
+	${MKDIR} -p ${OBJECTDIR}
+	$(COMPILE.c) -O2 -o ${OBJECTDIR}/libocarina.o libocarina.c
+
+# Subprojects
+.build-subprojects:
+
+# Clean Targets
+.clean-conf:
+	${RM} -r build/Release
+	${RM} dist/Release/${PLATFORM}/liblibocarina.a
+
+# Subprojects
+.clean-subprojects:
